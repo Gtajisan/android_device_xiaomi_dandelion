a88b0b9b426625c24c301c495108b007fe4b8521
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-13 22:09:03 +0600

    Bring up

--- Twrp_dandelion.mk
+++ Twrp_dandelion.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from dandelion device
 $(call inherit-product, device/xiaomi/dandelion/device.mk)
 
 PRODUCT_DEVICE := dandelion
-PRODUCT_NAME := omni_dandelion
+PRODUCT_NAME := twrp_dandelion
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 220233L2G
 PRODUCT_MANUFACTURER := xiaomi
