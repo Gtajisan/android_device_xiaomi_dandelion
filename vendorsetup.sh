7e97c1a357a7f6f25ac1e50701fb2b80e6391f22
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-13 22:12:26 +0600

    Brick soon

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_dandelion-user
-add_lunch_combo omni_dandelion-userdebug
-add_lunch_combo omni_dandelion-eng
+add_lunch_combo twrp_dandelion-user
+add_lunch_combo twrp_dandelion-userdebug
+add_lunch_combo twrp_dandelion-eng
